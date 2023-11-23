bfc78d77f5428d33275a1bb1c2268fc99c1f993a
maariraaj <maari000@hotmail.com>
2023-11-23 23:06:47 +0530

    231123

--- Guvi/React-Projects/List/list/src/App.jsx
+++ Guvi/React-Projects/List/list/src/App.jsx
@@ -28,6 +28,23 @@ function App() {
   return (
     <div>
       <h1>React-working with Lists!</h1>
+      <ul>
+        {list.map((item,index)=>{
+          return <li key={item.ObjID}>
+            <span>
+              <a href={item.url} target='_blank'>{item.title}</a>
+            </span>
+            <span></span>
+            <span>{item.points}</span>
+            </li>
+        })}
+      </ul>
+      <div className="card" id='main'>
+        <img src="..." class="card-img-top" alt="..."/>
+        <div className="card-body">
+          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>
     </div>
   );
 }
