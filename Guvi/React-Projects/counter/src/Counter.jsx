6bde6f3130f85723e479a8a3f754ab9b06b1b599
maariraaj <maari000@hotmail.com>
2023-11-27 22:40:47 +0530

    271123

--- Guvi/React-Projects/counter/src/Counter.jsx
+++ Guvi/React-Projects/counter/src/Counter.jsx
@@ -1,8 +1,23 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 function Counter() {
     const [count, setCount] = useState(0);
     const [bool, setBool] = useState(false);
+
+    useEffect(()=>{
+      console.log("Every Mount and Updated!!");
+    });
+
+    useEffect(()=>{
+      console.log("Every Mount because it has empty dependancy array!!");
+    }), []; 
+
+    useEffect(()=>{
+      console.log("Every Mount and bool dependancy array!!");
+
+      return ()=> console.log("Unmount!!!");
+    }, [bool]);
+    
 
   return (
     <div>
