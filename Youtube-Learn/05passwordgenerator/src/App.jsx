8f1b43cc6ce177392173276bde957ce4c8f817a1
maariraaj <maari000@hotmail.com>
2024-01-08 22:28:04 +0530

    080124

--- Youtube-Learn/05passwordgenerator/src/App.jsx
+++ Youtube-Learn/05passwordgenerator/src/App.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useState } from 'react'
+import { useCallback, useEffect, useRef, useState } from 'react'
 import './App.css'
 
 function App() {
@@ -7,18 +7,48 @@ function App() {
   const [charAllowed, setCharAllowed] = useState(false);
   const [password, setPassword] = useState('');
 
+  const passwordRef=useRef(null);
+
   const generatePassword=useCallback(()=>{
-    
-  })
+    let pass='';
+    let str='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+
+    if(numberAllowed) str += '0123456789';
+    if(charAllowed) str += '!@#$%^&*()_+';
+
+    for(let i=1;i<length;i++){
+      const char= Math.floor(Math.random() * str.length + 1);
+      pass += str.charAt(char)
+    }
+
+    setPassword(pass)
+
+  }, [length, numberAllowed, charAllowed])
+
+  useEffect(()=>{
+    generatePassword();
+  }, [length, numberAllowed, charAllowed])
+
+  const copyPasswordToClipboard=()=>{
+    window.navigator.clipboard.writeText(password);
+    passwordRef.current?.select();
+  }
   
 
   return (
     <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500">
       <h1 className="text-white text-center my-3">Password Generator</h1>
       <div className="flex shadow rounded-lg overflow-hidden mb-4">
-        <input type="text" className="outline-none w-full py-1 px-3" placeholder='Password' readOnly />
-        <button className='outline-npne bg-blue-700 text-white px-3 py-0.5 shrink-0'>Copy</button>
+        <input 
+        type="text" 
+        className="outline-none w-full py-1 px-3" 
+        placeholder='Password' 
+        value={password} 
+        readOnly
+        ref={passwordRef} />
+        <button className='outline-npne bg-blue-700 text-white px-3 py-0.5 shrink-0' onClick={copyPasswordToClipboard}>Copy</button>
       </div>
+
       <div className="flex items-center gap-x-1">
         <input 
         type='range' 
@@ -30,6 +60,7 @@ function App() {
         id='' />
         <label htmlFor="length">Length: {length}</label>
       </div>
+
       <div className="flex items-center gap-x-1">
         <input 
         type="checkbox"
@@ -41,6 +72,7 @@ function App() {
         id='' />
         <label htmlFor="number">Numbers</label>
       </div>
+
       <div className="flex items-center gap-x-1">
         <input 
         type="checkbox"
