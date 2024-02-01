282e99f7076df9bfc401704c236aabef1c552034
maariraaj <maari000@hotmail.com>
2024-02-01 21:38:30 +0530

    010224

--- Sharpner/oops.js
+++ Sharpner/oops.js
@@ -347,3 +347,35 @@
 //     return lastOccurrence;
 // }
 // console.log(searchRange([5, 7, 7, 8, 8, 10], 8));
+
+
+//Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
+
+// function search(nums, target){
+//     let l=0;
+//     let h=nums.length-1;
+
+//     while(l<=h) {
+//         let mid=Math.floor((l+h)/2);
+
+//         if(nums[mid]===target){
+//             return mid;
+//         }
+//         if(nums[l]<=nums[mid]){
+//             if(target>=nums[l] && target<=nums[mid]){
+//                 h=mid-1;
+//             }else{
+//                 l=mid+1;
+//             }
+//         }else{
+//             if(target>nums[mid] && target<=nums[h]){
+//                 l=mid+1;
+//             }else{
+//                 h=mid-1;
+//             }
+//         }
+//     }
+//     return -1;
+// }
+
+// console.log(search([6,7,0,1,2,4,5], 6));
