e48571a575434cdbe633adfd154344c89c7fd40c
maariraaj <maari000@hotmail.com>
2024-02-20 22:06:08 +0530

    200224

---
+++ Sharpner/stack.js
@@ -0,0 +1,54 @@
+// class Stack{
+//     constructor(){
+//         this.top=-1;
+//         this.stack=[];
+//     }
+//     push(ele){
+//         this.top=this.top+1;
+//         this.stack[this.top]=ele;
+//     }
+//     pop(){
+//         this.stack.pop();
+//         this.top=this.top-1;
+//     }
+//     peek(){
+//         return this.stack[this.top];
+//     }
+//     isEmpty(){
+//         if(this.top===-1){
+//             return true;
+//         }else{
+//             return false;
+//         }
+//     }
+// }
+// let x=new Stack;
+// console.log(x.isEmpty());
+// x.push(6);
+// x.push(9);
+// x.push(69);
+// x.push(96);
+// x.push(169);
+// x.pop();
+
+// console.log(x.stack);
+// console.log(x.peek());
+// console.log(x.isEmpty());
+
+
+// let y=[];
+// y.push(6);
+// y.push(9);
+// y.push(69);
+// y.push(96);
+// y.push(196);
+// y.pop();
+// console.log(y);
+// console.log(y[y.length-1]);
+// if(y.length===0){
+//     console.log("It is empty");
+// }else{
+//     console.log("It is not empty");
+// }
+
+
