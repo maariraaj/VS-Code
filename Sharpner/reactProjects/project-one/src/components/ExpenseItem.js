94f1d8278231930426939ce02993d8e54ab5ff89
maariraaj <maari000@hotmail.com>
2024-03-23 19:49:55 +0530

    Formatting Date - Giving Calendar Look

--- Sharpner/reactProjects/project-one/src/components/ExpenseItem.js
+++ Sharpner/reactProjects/project-one/src/components/ExpenseItem.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import './ExpenseItem.css';
 
+const months=['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 function ExpenseItem(props) {
+    const month=props.date.getMonth();
+    const year=props.date.getFullYear();
+    let date=props.date.getDate();
+    if(date<10){
+        date='0'+date;
+    }
     return (
     <div className="expense-item">
-        <div>{props.date.toISOString()}</div>
+        <div>
+            <div>{months[month]}</div>
+            <div>{year}</div>
+            <div>{date}</div>
+        </div>
         <div className='expense-item__location'>{props.location}</div>
         <div className="expense-item__description">
         <h2>{props.title}</h2>
@@ -14,4 +25,4 @@ function ExpenseItem(props) {
     );
 }
   
-  export default ExpenseItem;
+export default ExpenseItem;
