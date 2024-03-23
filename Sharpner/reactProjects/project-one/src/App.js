204b2c4d38700a140f286f0e01723d4a144f4db1
maariraaj <maari000@hotmail.com>
2024-03-23 15:10:25 +0530

    first custom components

--- Sharpner/reactProjects/project-one/src/App.js
+++ Sharpner/reactProjects/project-one/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import ExpenseItem from "./components/ExpenseItem";
 
 class App extends Component {
   render() {
     return (
       <div>
         <h1>Let's get Started</h1>
+        <ExpenseItem></ExpenseItem>
       </div>
     );
   }
---
+++ Sharpner/reactProjects/project-one/src/components/ExpenseItem.js
@@ -0,0 +1,11 @@
+import React from 'react'
+function ExpenseItem(){
+    return(
+      <div>
+        <div>Food - $5</div>
+        <div>Petrol - $20</div>
+        <div>Movie - $10</div>
+      </div>
+    )
+  }
+  export default ExpenseItem;
