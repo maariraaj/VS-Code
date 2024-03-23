a001855ce528a8ce5c565e262209761b4edecd4b
maariraaj <maari000@hotmail.com>
2024-03-23 16:13:24 +0530

    complex JSX

--- Sharpner/reactProjects/project-one/src/components/ExpenseItem.js
+++ Sharpner/reactProjects/project-one/src/components/ExpenseItem.js
@@ -1,11 +1,26 @@
 import React from 'react'
+// function ExpenseItem(){
+//     return(
+//       <div>
+//         <div>Food - $5</div>
+//         <div>Petrol - $20</div>
+//         <div>Movie - $10</div>
+//       </div>
+//     )
+//   }
+//   export default ExpenseItem;
+
+
 function ExpenseItem(){
-    return(
+    return (
       <div>
-        <div>Food - $5</div>
-        <div>Petrol - $20</div>
-        <div>Movie - $10</div>
+        <div>15 August 2023</div>
+        <div>
+          <h2>Book</h2>
+          <div>$10</div>
+        </div>     
+        <div>Delhi</div>
       </div>
-    )
+    );
   }
   export default ExpenseItem;
