44fd6ff6eb30c7be68c0216f001c6db2004b8561
maariraaj <maari000@hotmail.com>
2024-03-23 20:51:03 +0530

    Splitting the Components

---
+++ Sharpner/reactProjects/project-one/src/components/ExpenseDate.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './ExpenseDate.css';
+
+const months=['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+function ExpenseDate(props) {
+    const month=props.calenderDate.getMonth();
+    const year=props.calenderDate.getFullYear();
+    let date=props.calenderDate.getDate();
+    if(date<10){
+        date='0'+date;
+    }
+  return (
+    <div className='expense-date'>
+        <div className='expense-date__month'>{months[month]}</div>
+        <div className='expense-date__year'>{year}</div>
+        <div className='expense-date__date'>{date}</div>
+    </div>
+  );
+}
+
+export default ExpenseDate
--- Sharpner/reactProjects/project-one/src/components/ExpenseItem.js
+++ Sharpner/reactProjects/project-one/src/components/ExpenseItem.js
@@ -1,21 +1,11 @@
 import React from 'react';
 import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
 
-const months=['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 function ExpenseItem(props) {
-    const month=props.date.getMonth();
-    const year=props.date.getFullYear();
-    let date=props.date.getDate();
-    if(date<10){
-        date='0'+date;
-    }
     return (
     <div className="expense-item">
-        <div>
-            <div>{months[month]}</div>
-            <div>{year}</div>
-            <div>{date}</div>
-        </div>
+        <ExpenseDate calenderDate={props.date}></ExpenseDate>
         <div className='expense-item__location'>{props.location}</div>
         <div className="expense-item__description">
         <h2>{props.title}</h2>
