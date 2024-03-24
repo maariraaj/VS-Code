f5b1dbbaefcccb947f6c33cad1da85b6c9dcc6b1
maariraaj <maari000@hotmail.com>
2024-03-24 12:41:01 +0530

    mar24

--- Sharpner/reactProjects/project-one/src/components/Expenses/ExpenseItem.js
+++ Sharpner/reactProjects/project-one/src/components/Expenses/ExpenseItem.js
@@ -9,8 +9,8 @@ const ExpenseItem=(props) => {
         <ExpenseDate calenderDate={props.date}/>
         <div className='expense-item__location'>{props.location}</div>
         <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <div className="expense-item__price">${props.price}</div>
+            <h2>{props.title}</h2>
+            <div className="expense-item__price">${props.price}</div>
         </div>
     </Card>
     );
