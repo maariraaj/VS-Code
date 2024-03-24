2ae5600f97fadf7c006f7ad293c2c167558f5eb9
maariraaj <maari000@hotmail.com>
2024-03-24 17:26:30 +0530

    Manipulating the DOM - React States

--- Sharpner/reactProjects/project-one/src/App.js
+++ Sharpner/reactProjects/project-one/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Expenses from './components/Expenses/Expenses';
 
 const expenses=[
-  {id:"1", date:new Date(2023, 7, 15), title:"Insurance", price:"100", location:'Bangalore' },
+  {id:"1", date:new Date(2023, 7, 15), title:"Insurance", price:"300", location:'Bangalore' },
   {id:"2", date:new Date(2023, 3, 25), title:"Book", price:"10", location:'Delhi' },
   {id:"3", date:new Date(2023, 10, 11), title:"Pen", price:"1", location:'Hyderabad' },
   {id:"4", date:new Date(2023, 1, 14), title:"Laptop", price:"200", location:'Mumbai' }
--- Sharpner/reactProjects/project-one/src/components/Expenses/ExpenseItem.js
+++ Sharpner/reactProjects/project-one/src/components/Expenses/ExpenseItem.js
@@ -1,21 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 
 const ExpenseItem=(props) => {
-    const handleForDelete=() => {
-        console.log("Clicked")
-    };
+
+    const [price, setPrice]=useState(props.price);
+
+    const handleForUpdate=()=>{
+        setPrice("100");
+    }
+
     return (
     <Card className="expense-item">
         <ExpenseDate calenderDate={props.date}/>
         <div className='expense-item__location'>{props.location}</div>
         <div className="expense-item__description">
             <h2>{props.title}</h2>
-            <div className="expense-item__price">${props.price}</div>
-            <button onClick={handleForDelete}>Delete Expense</button>
+            <div className="expense-item__price">${price}</div>
         </div>
+        <button onClick={handleForUpdate}>Change Expense</button>
     </Card>
     );
 }
