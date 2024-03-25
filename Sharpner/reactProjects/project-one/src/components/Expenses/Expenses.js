74b1f1577f5ba6db09e5dab9e344a08eff019735
maariraaj <maari000@hotmail.com>
2024-03-25 22:01:16 +0530

    Implementing the filter feature

--- Sharpner/reactProjects/project-one/src/components/Expenses/Expenses.js
+++ Sharpner/reactProjects/project-one/src/components/Expenses/Expenses.js
@@ -10,11 +10,14 @@ const Expenses=(props) => {
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
+  const filteredExpenses=props.expenses.filter((expense)=>{
+    return expense.date.getFullYear().toString()===filteredYear;
+  })
 
   return (
     <Card className='expenses'>
       <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-      {props.expenses.map((expense)=>{
+      {filteredExpenses.map((expense)=>{
           return(
             <ExpenseItem
               key={expense.id}
