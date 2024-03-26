5e82d13cf790b6f2211d58e6db7295829729c087
maariraaj <maari000@hotmail.com>
2024-03-26 14:27:39 +0530

    Chart App

--- Sharpner/reactProjects/project-one/src/components/Expenses/Expenses.js
+++ Sharpner/reactProjects/project-one/src/components/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import './Expenses.css';
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
 import ExpensesList from './ExpensesList';
+import ExpensesChart from './ExpensesChart';
 
 const Expenses=(props) => {
   const [filteredYear, setFilteredYear] = useState('2022');
@@ -17,6 +18,7 @@ const Expenses=(props) => {
   return (
     <Card className='expenses'>
       <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+      <ExpensesChart expenses={filteredExpenses}/>
       <ExpensesList expenses={filteredExpenses}/>
     </Card>
   )
