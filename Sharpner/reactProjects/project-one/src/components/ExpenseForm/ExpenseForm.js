c3ae73c1fa50edb4f9e2d01e8efe592d5a03e681
maariraaj <maari000@hotmail.com>
2024-03-26 13:01:08 +0530

    DIY - Challenges on Conditional Rendering

--- Sharpner/reactProjects/project-one/src/components/ExpenseForm/ExpenseForm.js
+++ Sharpner/reactProjects/project-one/src/components/ExpenseForm/ExpenseForm.js
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 import './ExpenseForm.css';
 
 const ExpenseForm = (props) => {
+
+    const [isEditing, setIsEditing]=useState(false);
+
     const [enteredTitle, setEnteredTitle]=useState('');
     const [enteredAmount, setEnteredAmount]=useState('');
     const [enteredDate, setEnteredDate]=useState('');
@@ -16,11 +19,18 @@ const ExpenseForm = (props) => {
             location: enteredLocation
         }
         props.onAddExpense(expenseData);
+        setIsEditing(false);
 
         setEnteredTitle('');
         setEnteredAmount('');
         setEnteredDate('');
         setEnteredLocation('');
+    };
+    const startEditingHandler=()=>{
+        setIsEditing(true);
+    }
+    const stopIsEditingHandler=()=>{
+        setIsEditing(false);
     };
     const titleChangeHandler=(event)=>{
         setEnteredTitle(event.target.value);
@@ -36,47 +46,54 @@ const ExpenseForm = (props) => {
     };
 
   return (
-    <form className='new-expense' onSubmit={submitHandler}>
-        <div className='new-expense__controls'>
-            <div className='new-expense__control'>
-                <label>Title</label>
-                <input 
-                    type='text' 
-                    onChange={titleChangeHandler} 
-                    value={enteredTitle} />
-            </div>
+    <div className='new-expense'>
+        {!isEditing && (
+            <button onClick={startEditingHandler}>Add New Expense</button>
+        )}
+        {isEditing && (
+            <form onSubmit={submitHandler}>
+                <div className='new-expense__controls'>
+                    <div className='new-expense__control'>
+                        <label>Title</label>
+                        <input
+                            type='text'
+                            onChange={titleChangeHandler}
+                            value={enteredTitle} />
+                    </div>
 
-            <div className='new-expense__control'>
-                <label>Amount</label>
-                <input 
-                    type='number' 
-                    onChange={amountChangeHandler} 
-                    value={enteredAmount} />
-            </div>
+                    <div className='new-expense__control'>
+                        <label>Amount</label>
+                        <input
+                            type='number'
+                            onChange={amountChangeHandler}
+                            value={enteredAmount} />
+                    </div>
 
-            <div className='new-expense__control'>
-                <label>Date</label>
-                <input 
-                    type='date' 
-                    min='2021-01-01'
-                    max='2024-12-31'
-                    onChange={dateChangeHandler} 
-                    value={enteredDate} />
-            </div>
+                    <div className='new-expense__control'>
+                        <label>Date</label>
+                        <input
+                            type='date'
+                            min='2021-01-01'
+                            max='2024-12-31'
+                            onChange={dateChangeHandler}
+                            value={enteredDate} />
+                    </div>
 
-            <div className='new-expense__control'>
-                <label>Location</label>
-                <input 
-                    type='text' 
-                    onChange={locationChangeHandler} 
-                    value={enteredLocation} />
-            </div>
-        </div>
-        <div className='new-expense__actions'>
-            <button type='submit'>Add Expense</button>
-        </div>
-    </form>
-  )
-}
+                    <div className='new-expense__control'>
+                        <label>Location</label>
+                        <input
+                            type='text'
+                            onChange={locationChangeHandler}
+                            value={enteredLocation} />
+                    </div>
+                </div>
+                <div className='new-expense__actions'>
+                    <button type='button' onClick={stopIsEditingHandler}>Cancel</button>
+                    <button type='submit'>Add Expense</button>
+                </div>
+            </form>
+        )}
+    </div>
+  )}
 
 export default ExpenseForm
