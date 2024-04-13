5b96256073d850ba01d7a54d0d0257d7ca06f37d
maariraaj <maari000@hotmail.com>
2024-04-13 21:24:40 +0530

    temp

--- Sharpner/reactProjects/reactRouter/src/App.jsx
+++ Sharpner/reactProjects/reactRouter/src/App.jsx
@@ -1,14 +1,22 @@
-import {RouterProvider, createBrowserRouter} from 'react-router-dom';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import HomePage from './pages/Home';
 import ProductsPage from './pages/Products';
+import RootLayout from './pages/Root';
 
-const router=createBrowserRouter([
-  {path:'', element: <HomePage />},
-  {path: '/products', element: <ProductsPage />}
-])
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    children: [
+      { path: '/', element: <HomePage /> },
+      { path: '/products', element: <ProductsPage /> }
+    ]
+  }
+]);
+
 const App = () => {
   return (
-    <RouterProvider router={router}/>                                   
+    <RouterProvider router={router} />
   )
 }
 
