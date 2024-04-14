9c788564eb75cb768326b10c08798771aba47db9
maariraaj <maari000@hotmail.com>
2024-04-14 22:16:04 +0530

    Making Api calls in React

--- Sharpner/reactProjects/fetchAPI/src/App.jsx
+++ Sharpner/reactProjects/fetchAPI/src/App.jsx
@@ -1,9 +1,37 @@
-import './App.css'
+import React, { useState } from 'react';
 
-const App = () => {
+import MoviesList from './components/MoviesList.jsx';
+import './App.css';
+
+function App() {
+  const [movies, setMovies] = useState([]);
+
+  async function fetchMovieHandler() {
+    try {
+      const response = await fetch('https://swapi.dev/api/films/');
+      const data = await response.json();
+      const transformedMovies = data.results.map((movieData) => ({
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        releaseDate: movieData.release_date
+      }));
+      setMovies(transformedMovies);
+    } catch (error) {
+      console.error('Error fetching movies:', error);
+    }
+  };
+
   return (
-    <div>App</div>
-  )
+    <React.Fragment>
+      <section>
+        <button onClick={fetchMovieHandler}>Fetch Movies</button>
+      </section>
+      <section>
+        <MoviesList movies={movies} />
+      </section>
+    </React.Fragment>
+  );
 }
 
-export default App
+export default App;
