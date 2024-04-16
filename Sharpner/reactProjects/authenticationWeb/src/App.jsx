551f8dd82e308df068a1b8cfe04339cc02a6f7da
maariraaj <maari000@hotmail.com>
2024-04-16 16:04:26 +0530

    Addressing security and logout issues

--- Sharpner/reactProjects/authenticationWeb/src/App.jsx
+++ Sharpner/reactProjects/authenticationWeb/src/App.jsx
@@ -1,22 +1,31 @@
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 import Layout from './components/Layout/Layout.jsx';
 import UserProfile from './components/Profile/UserProfile.jsx';
 import AuthPage from './pages/AuthPage.jsx';
 import HomePage from './pages/HomePage.jsx';
+import { useContext } from 'react';
+import AuthContext from './store/auth-context.jsx';
 
 function App() {
+  const authCtx = useContext(AuthContext);
+
   return (
     <Layout>
       <Switch>
         <Route path='/' exact>
           <HomePage />
         </Route>
-        <Route path='/auth'>
-          <AuthPage />
-        </Route>
+        {!authCtx.isLoggedIn && (
+          <Route path='/auth'>
+            <AuthPage />
+          </Route>)}
         <Route path='/profile'>
-          <UserProfile />
+          {authCtx.isLoggedIn && <UserProfile />}
+          {!authCtx.isLoggedIn && <Redirect to='/auth' />}
+        </Route>
+        <Route path='*'>
+          <Redirect to='/' />
         </Route>
       </Switch>
     </Layout>
