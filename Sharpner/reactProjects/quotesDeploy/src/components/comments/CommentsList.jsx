16f16250699ce3df64f0d2ad6a0792c883118395
maariraaj <maari000@hotmail.com>
2024-04-21 14:48:50 +0530

    temp

---
+++ Sharpner/reactProjects/quotesDeploy/src/components/comments/CommentsList.jsx
@@ -0,0 +1,14 @@
+import CommentItem from './CommentItem.jsx';
+import classes from './CommentsList.module.css';
+
+const CommentsList = (props) => {
+  return (
+    <ul className={classes.comments}>
+      {props.comments.map((comment) => (
+        <CommentItem key={comment.id} text={comment.text} />
+      ))}
+    </ul>
+  );
+};
+
+export default CommentsList;
