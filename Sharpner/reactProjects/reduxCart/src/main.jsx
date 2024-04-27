0ea8ce7ba04233c811b98ffe48ac74d22bd9bc51
maariraaj <maari000@hotmail.com>
2024-04-27 21:44:28 +0530

    Shoping page app using reducers

--- Sharpner/reactProjects/reduxCart/src/main.jsx
+++ Sharpner/reactProjects/reduxCart/src/main.jsx
@@ -1,7 +1,13 @@
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 
 import './index.css';
 import App from './App.jsx';
+import store from './store/index.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+);
