4e08442f41801541a2e50760b9509560ff89a664
Mariraj <maari000@hotmail.com>
2024-05-27 16:16:22 +0530

    Login Screen

--- Sharpner/reactProjects/mailbox-client-app/src/App.js
+++ Sharpner/reactProjects/mailbox-client-app/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import SignUp from './components/authentication/SignUp';
+import SignIn from './components/authentication/SignIn';
 import Home from './components/Home';
 
 const App = () => {
   return (
     <Router>
       <Routes>
+        <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/" element={<Home />} />
       </Routes>
--- Sharpner/reactProjects/mailbox-client-app/src/store/store.jsx
+++ Sharpner/reactProjects/mailbox-client-app/src/store/store.jsx
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import signinReducer from './signin-slice';
 import signupReducer from './signup-slice';
 
 const store = configureStore({
   reducer: {
+    signin: signinReducer,
     signup: signupReducer,
   },
 });
