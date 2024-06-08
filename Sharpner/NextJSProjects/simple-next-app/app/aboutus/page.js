78169e5f6135a729efffd738895166ac897b70f0
Mariraj <maari000@hotmail.com>
2024-06-08 19:52:03 +0530

    Final touch to About US page

--- Sharpner/NextJSProjects/simple-next-app/app/aboutus/page.js
+++ Sharpner/NextJSProjects/simple-next-app/app/aboutus/page.js
@@ -1,3 +1,15 @@
+import Link from "next/link";
+import { Fragment } from "react";
+
 export default function Page() {
-    return <h1>Hello, About Us Page!</h1>
-}
+    return (
+        <Fragment>
+            <h1>Hello, About Us Page!</h1>
+            <ul>
+                <li><Link href="/aboutus/1">Yash</Link></li>
+                <li><Link href="/aboutus/2">Vaibhav</Link></li>
+                <li><Link href="/aboutus/3">Suresh</Link></li>
+            </ul>
+        </Fragment>
+    )
+};
