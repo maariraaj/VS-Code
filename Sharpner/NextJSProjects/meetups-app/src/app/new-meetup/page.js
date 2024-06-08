56524a38e56e1ec784621d545d2008b1dc34f6d1
Mariraj <maari000@hotmail.com>
2024-06-08 22:37:53 +0530

    Dynamic routing [DIY}

---
+++ Sharpner/NextJSProjects/meetups-app/src/app/new-meetup/page.js
@@ -0,0 +1,10 @@
+'use client';
+
+import NewMeetupForm from "@/components/meetups/NewMeetupForm";
+
+export default function NewMeetupPage() {
+    function addMeetupHandler(enteredMeetupData) {
+        console.log(enteredMeetupData);
+    }
+    return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
+}
