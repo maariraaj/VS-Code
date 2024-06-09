745bb3eb1295d7ddc605fef1ba1bb7bd33f6575f
Mariraj <maari000@hotmail.com>
2024-06-09 22:31:35 +0530

    Prerendering the powers of Next JS

--- Sharpner/NextJSProjects/react-meetups-app/pages/[meetupId]/index.js
+++ Sharpner/NextJSProjects/react-meetups-app/pages/[meetupId]/index.js
@@ -1,6 +1,6 @@
 import MeetupDetail from '../../components/meetups/MeetupDetail';
 
-function MeetupDetails() {
+function MeetupDetails(props) {
   return (
     <MeetupDetail
       image='https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg'
@@ -10,5 +10,40 @@ function MeetupDetails() {
     />
   );
 }
+
+export async function getStaticPaths() {
+  return {
+    fallback: true,
+    paths: [
+      {
+        params: {
+          meetupId: 'm1'
+        },
+      },
+      {
+        params: {
+          meetupId: 'm2'
+        },
+      }
+    ]
+  }
+};
+
+export async function getStaticProps(context) {
+  const meetupId = context.params.meetupId;
+  console.log(meetupId);
+  return {
+    props: {
+      meetupData: {
+        id: meetupId,
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
+        title: 'First Meetup',
+        address: 'Some Street 5, Some City',
+        description: 'This is a first meetup'
+      }
+    },
+    revalidate: 1
+  };
+};
 
 export default MeetupDetails;
