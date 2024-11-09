d49a7a72a2e69bfeeb54f93c0e71840d562ee357
Mariraj <maari000@hotmail.com>
2024-11-09 19:30:16 +0530

    Arrays , spread and rest operators

--- Sharpner/nodeJS/first-app.js
+++ Sharpner/nodeJS/first-app.js
@@ -4,17 +4,25 @@
 // console.log('Hello World');
 
 
-const product = (a, b) => {
-    return a * b;
-}
-console.log(product(5, 1000));
+// const product = (a, b) => {
+//     return a * b;
+// }
+// console.log(product(5, 1000));
 
 
-const student = {
-    name: 'Raj',
-    age: '29',
-    greet() {
-        console.log('Hi, I am ' + this.name);
-    }
-}
-student.greet();
+// const student = {
+//     name: 'Raj',
+//     age: '29',
+//     greet() {
+//         console.log('Hi, I am ' + this.name);
+//     }
+// }
+// student.greet();
+
+
+
+const arr = ['apple', 'oranges', ' ', 'mango', ' ', 'lemon'];
+const newArr = arr.map(item => {
+    return item === ' ' ? 'empty string' : item;
+});
+console.log(newArr);
