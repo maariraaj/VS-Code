b11bcd376c4dfa226542d5324233326c71fe9ca1
Mariraj <maari000@hotmail.com>
2024-11-09 18:18:50 +0530

    Lets Revise Javascript concepts

--- Sharpner/nodeJS/first-app.js
+++ Sharpner/nodeJS/first-app.js
@@ -1,4 +1,20 @@
 // const fs = require('fs');
 // fs.writeFileSync('hello.txt', 'Hello from Node.JS');
 
-console.log('Hello World');
+// console.log('Hello World');
+
+
+const product = (a, b) => {
+    return a * b;
+}
+console.log(product(5, 1000));
+
+
+const student = {
+    name: 'Raj',
+    age: '29',
+    greet() {
+        console.log('Hi, I am ' + this.name);
+    }
+}
+student.greet();
