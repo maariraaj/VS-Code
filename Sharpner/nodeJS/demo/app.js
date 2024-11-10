fb802dda2ebdc8ae182dfb82e5077fa97e31234e
Mariraj <maari000@hotmail.com>
2024-11-10 13:43:44 +0530

    Request / Response

--- Sharpner/nodeJS/demo/app.js
+++ Sharpner/nodeJS/demo/app.js
@@ -1,5 +1,44 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
-    console.log('Mariraj');
+    console.log(req.url, req.method, req.headers);
+    //process.exit();
+    const url = req.url;
+    res.setHeader('Conten-Type', 'text/html');
+    if (url === '/') {
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"/><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
+    if (url === '/home') {
+        res.write('<html>');
+        res.write('<head><title>My First Page</title></head>');
+        res.write('<body><h1>Welcome Home</h1></body>');
+        res.write('</html>');
+        res.end();
+        return res.end();
+    }
+    if (url === '/about') {
+        res.write('<html>');
+        res.write('<head><title>My First Page</title></head>');
+        res.write('<body><h1>Welcome to About Us page</h1></body>');
+        res.write('</html>');
+        res.end();
+        return res.end();
+    }
+    if (url === '/node') {
+        res.write('<html>');
+        res.write('<head><title>My First Page</title></head>');
+        res.write('<body><h1>Welcome to My Node JS Project</h1></body>');
+        res.write('</html>');
+        res.end();
+        return res.end();
+    }
+    res.write('<html>');
+    res.write('<head><title>My First Page</title></head>');
+    res.write('<body><h1>Welcome to my Node Js project</h1></body>');
+    res.write('</html>');
+    res.end();
 });
 server.listen(4000);
