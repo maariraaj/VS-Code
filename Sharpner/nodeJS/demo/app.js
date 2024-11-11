7c71e8d24f06a8c3a8746122413d067136b29b93
Mariraj <maari000@hotmail.com>
2024-11-11 12:18:51 +0530

    Lets get started with Express

--- Sharpner/nodeJS/demo/app.js
+++ Sharpner/nodeJS/demo/app.js
@@ -1,7 +1,24 @@
-const http = require('http');
-const routes = require('./routes');
+// const http = require('http');
+// const routes = require('./routes');
 
-console.log(routes.someText);
+// console.log(routes.someText);
 
-const server = http.createServer(routes.handler);
-server.listen(4000);
+// const server = http.createServer(routes.handler);
+// server.listen(4000);
+
+
+
+const express = require('express');
+
+const app = express();
+
+app.use((req, res, next) => {
+    console.log('In the middleware');
+    next();
+})
+app.use((req, res, next) => {
+    console.log('In another middleware');
+    res.send('<h1>Hello from Express!</h1>');
+})
+
+app.listen(4000);
