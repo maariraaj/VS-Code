3ada0f5944030d21dd9f81d76d0cd44aabfbb554
Mariraj <maari000@hotmail.com>
2024-11-11 14:16:52 +0530

    Intro to Middlewares

--- Sharpner/nodeJS/demo/app.js
+++ Sharpner/nodeJS/demo/app.js
@@ -9,15 +9,24 @@
 
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('In the middleware');
-    next();
+app.use(bodyParser.urlencoded({ extended: false }));
+// app.use('/', (req, res, next) => {
+//     //console.log('This always runs.');
+//     next();
+// })
+app.use('/add-product', (req, res, next) => {
+    //console.log('In another middleware');
+    res.send('<form action="/product" method="POST"><input type="text" name="title" placeholder="Product name"><input type="text" name="size"placeholder="Size"><button type="submit">Add Product</button></form>');
 })
-app.use((req, res, next) => {
-    console.log('In another middleware');
+app.use('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+})
+app.use('/', (req, res, next) => {
     res.send('<h1>Hello from Express!</h1>');
 })
 
