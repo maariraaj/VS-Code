f628beddc9e4417646f0c1a19fcda68435628935
Mariraj <maari000@hotmail.com>
2024-11-11 15:22:05 +0530

    Routers and Filters

--- Sharpner/nodeJS/demo/app.js
+++ Sharpner/nodeJS/demo/app.js
@@ -1,5 +1,5 @@
 // const http = require('http');
-// const routes = require('./routes');
+// const routes = require('./routes1');
 
 // console.log(routes.someText);
 
@@ -10,24 +10,19 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
-// app.use('/', (req, res, next) => {
-//     //console.log('This always runs.');
-//     next();
-// })
-app.use('/add-product', (req, res, next) => {
-    //console.log('In another middleware');
-    res.send('<form action="/product" method="POST"><input type="text" name="title" placeholder="Product name"><input type="text" name="size"placeholder="Size"><button type="submit">Add Product</button></form>');
-})
-app.use('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-})
-app.use('/', (req, res, next) => {
-    res.send('<h1>Hello from Express!</h1>');
+
+app.use('/admin', adminRoutes);
+
+app.use('/shop', shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found</h1>');
 })
 
 app.listen(4000);
