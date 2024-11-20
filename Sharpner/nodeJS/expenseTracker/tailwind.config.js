7d341cf7f7aeda6dd8dde966d41811680d5f4d88
Mariraj <maari000@hotmail.com>
2024-11-20 13:26:18 +0530

    Creating Signup Screen Frontend

---
+++ Sharpner/nodeJS/expenseTracker/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    './views/auth/*.html',
+    './views/**/*.html',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'teal-600': '#009688',
+      },
+    },
+  },
+  plugins: [],
+}
