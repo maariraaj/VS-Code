9dc77a4f86e2aacb3f181cd3387e5d8f3b952ed3
Mariraj <maari000@hotmail.com>
2024-11-22 09:49:01 +0530

    temp

---
+++ Sharpner/nodeJS/expenseTracker/controllers/auth.js
@@ -0,0 +1,47 @@
+const bcrypt = require("bcrypt");
+const User = require("../models/user");
+
+exports.postSignUp = async (req, res) => {
+    const { name, email, password } = req.body;
+    if (!email || !password || !name) {
+        return res.status(400).json({ error: "All fields are required" });
+    }
+    try {
+        const existingUser = await User.findOne({ where: { email } });
+
+        if (existingUser) {
+            return res.status(409).json({ error: "User already exists" });
+        }
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const newUser = await User.create({ name, email, password: hashedPassword });
+
+        res.status(201).json({ message: "User created successfully", user: newUser });
+    } catch (error) {
+        console.error("Error creating user:", error);
+        res.status(500).json({ error: "Internal server error" });
+    }
+};
+
+exports.postLogIn = async (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return res.status(400).json({ error: "Email and password are required" });
+    }
+
+    try {
+        const user = await User.findOne({ where: { email } });
+        if (!user) {
+            return res.status(404).json({ error: "User not found" });
+        }
+        const isPasswordMatch = await bcrypt.compare(password, user.password);
+
+        if (!isPasswordMatch) {
+            return res.status(401).json({ error: "User not authorized" });
+        }
+        return res.status(200).json({ message: "User login successful", user: { id: user.id, name: user.name } });
+    } catch (error) {
+        console.error("Error during login:", error);
+        res.status(500).json({ error: "Internal server error" });
+    }
+};
--- Sharpner/nodeJS/expenseTracker/routes/userRoutes.js
+++ Sharpner/nodeJS/expenseTracker/routes/userRoutes.js
@@ -1,49 +1,8 @@
 const express = require("express");
-const bcrypt = require("bcrypt");
 const router = express.Router();
-const User = require("../models/user");
+const authController = require('../controllers/auth');
 
-router.post("/signUp", async (req, res) => {
-    const { name, email, password } = req.body;
-    if (!email || !password || !name) {
-        return res.status(400).json({ error: "All fields are required" });
-    }
-    try {
-        const existingUser = await User.findOne({ where: { email } });
+router.post("/signUp", authController.postSignUp);
+router.post("/logIn", authController.postLogIn);
 
-        if (existingUser) {
-            return res.status(409).json({ error: "User already exists" });
-        }
-        const hashedPassword = await bcrypt.hash(password, 10);
-        const newUser = await User.create({ name, email, password: hashedPassword });
-
-        res.status(201).json({ message: "User created successfully", user: newUser });
-    } catch (error) {
-        console.error("Error creating user:", error);
-        res.status(500).json({ error: "Internal server error" });
-    }
-});
-router.post("/logIn", async (req, res) => {
-    const { email, password } = req.body;
-
-    if (!email || !password) {
-        return res.status(400).json({ error: "Email and password are required" });
-    }
-
-    try {
-        const user = await User.findOne({ where: { email } });
-        if (!user) {
-            return res.status(404).json({ error: "User not found" });
-        }
-        const isPasswordMatch = await bcrypt.compare(password, user.password);
-
-        if (!isPasswordMatch) {
-            return res.status(401).json({ error: "User not authorized" });
-        }
-        return res.status(200).json({ message: "User login successful", user: { id: user.id, name: user.name } });
-    } catch (error) {
-        console.error("Error during login:", error);
-        res.status(500).json({ error: "Internal server error" });
-    }
-});
 module.exports = router;
