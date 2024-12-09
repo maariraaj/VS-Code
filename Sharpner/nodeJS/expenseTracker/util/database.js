c7ae07ced628322e18eb7c7de90189072b815b44
Mariraj <maari000@hotmail.com>
2024-12-09 21:51:58 +0530

    temp

--- Sharpner/nodeJS/expenseTracker/util/database.js
+++ Sharpner/nodeJS/expenseTracker/util/database.js
@@ -1,7 +1,8 @@
 const { Sequelize } = require("sequelize");
+require('dotenv').config();
 
-const sequelize = new Sequelize('expense-tracker', 'root', 'Qwerty123', {
-    host: "localhost",
+const sequelize = new Sequelize(process.env.DB_SCHEMA, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
     dialect: "mysql",
 });
 
