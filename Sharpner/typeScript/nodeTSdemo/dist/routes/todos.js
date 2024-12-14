513d47c199af7618c56245efd12f0b46e41cd826
Mariraj <maari000@hotmail.com>
2024-12-14 17:59:41 +0530

    Typescript showdown

--- Sharpner/typeScript/nodeTSdemo/dist/routes/todos.js
+++ Sharpner/typeScript/nodeTSdemo/dist/routes/todos.js
@@ -7,24 +7,28 @@ router.get("/", (req, res, next) => {
     res.status(200).json({ todos: todos });
 });
 router.post("/todo", (req, res, next) => {
+    const body = req.body;
     const newTodo = {
         id: new Date().toISOString(),
-        text: req.body.text,
+        text: body.text,
     };
     todos.push(newTodo);
     res.status(201).json({ message: "Added todo", todo: newTodo, todos: todos });
 });
 router.put("/todo/:todoId", (req, res, next) => {
-    const tId = req.params.todoId;
+    const params = req.params;
+    const tId = params.todoId;
+    const body = req.body;
     const todoIndex = todos.findIndex((todoItem) => todoItem.id === tId);
     if (todoIndex >= 0) {
-        todos[todoIndex] = { id: todos[todoIndex].id, text: req.body.text };
+        todos[todoIndex] = { id: todos[todoIndex].id, text: body.text };
         res.status(200).json({ message: "Updated todo", todos: todos });
     }
     res.status(404).json({ message: "Item not found." });
 });
 router.delete("/todo/:todoId", (req, res, next) => {
-    todos = todos.filter((todoItem) => todoItem.id !== req.params.todoId);
+    const params = req.params;
+    todos = todos.filter((todoItem) => todoItem.id !== params.todoId);
     res.status(200).json({ message: "Deleted todo", todos: todos });
 });
 exports.default = router;
