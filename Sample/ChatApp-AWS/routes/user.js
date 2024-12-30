1805c4b2728caaada75ed4f13f55c053b906a2fd
Mariraj <maari000@hotmail.com>
2024-12-30 16:34:31 +0530

    temp

--- Sample/ChatApp-AWS/routes/user.js
+++ Sample/ChatApp-AWS/routes/user.js
@@ -7,24 +7,24 @@ const multerMiddleware = require('../middleware/multer')
 const upload = multerMiddleware.multer.single('image');
 const router = express.Router();
 
-router.post('/signup',userControler.userSignup);
-router.post('/signin',userControler.userSignin);
-router.post('/forgotpassword',passwordController.userResetpasswordMail)
+router.post('/signup', userControler.userSignup);
+router.post('/signin', userControler.userSignin);
+router.post('/forgotpassword', passwordController.userResetpasswordMail)
 router.get('/reset/:forgotId', passwordController.userResetpasswordform)
-router.post('/password-reset',passwordController.userResetpassword)
+router.post('/password-reset', passwordController.userResetpassword)
 
-router.post('/post-message',authController.authorization,userControler.saveChatHistory)
-router.post('/post-image',authController.authorization,upload,userControler.saveChatImages)
-router.get('/get-message',authController.authorization,userControler.getUserChatHistory);
-router.get('/get-users',authController.authorization,userControler.getAlluser)
-router.get('/get-user',authController.authorization,userControler.getcurrentuser)
-router.get('/get-messages',userControler.getAllChatHistory);
-router.post('/create-group',authController.authorization,userControler.createGroup)
-router.post('/update-group',authController.authorization,userControler.updateGroup)
-router.get('/get-groups',userControler.getAllgroups)
-router.get('/get-mygroups',authController.authorization,userControler.getMygroups)
-router.get('/get-group',userControler.getGroupbyId)
-router.get('/get-group-messages',userControler.getGroupChatHistory)
-router.get('/get-group-members',userControler.getGroupMembersbyId)
-router.get('/',mainPagecontroler.getMainpage)
+router.post('/post-message', authController.authorization, userControler.saveChatHistory)
+router.post('/post-image', authController.authorization, upload, userControler.saveChatImages)
+router.get('/get-message', authController.authorization, userControler.getUserChatHistory);
+router.get('/get-users', authController.authorization, userControler.getAlluser)
+router.get('/get-user', authController.authorization, userControler.getcurrentuser)
+router.get('/get-messages', userControler.getAllChatHistory);
+router.post('/create-group', authController.authorization, userControler.createGroup)
+router.post('/update-group', authController.authorization, userControler.updateGroup)
+router.get('/get-groups', userControler.getAllgroups)
+router.get('/get-mygroups', authController.authorization, userControler.getMygroups)
+router.get('/get-group', userControler.getGroupbyId)
+router.get('/get-group-messages', userControler.getGroupChatHistory)
+router.get('/get-group-members', userControler.getGroupMembersbyId)
+router.get('/', mainPagecontroler.getMainpage)
 module.exports = router;
